a925f6cde05e82f1e239de1887d949283f05e314
SeanYeoTE <tseensean@gmail.com>
2025-01-22 14:12:27 +0800

    ex00

---
+++ ex00/megaphone.cpp
@@ -0,0 +1,31 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   megaphone.cpp                                      :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: seayeo <seayeo@42.sg>                      +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/01/22 13:53:41 by seayeo            #+#    #+#             */
+/*   Updated: 2025/01/22 14:10:11 by seayeo           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <iostream>
+
+int main(int argc, char **argv) {
+	if (argc == 1)
+		std::cout << "* LOUD AND UNBEARABLE FEEDBACK NOISE *" << std::endl;
+	else {
+		for (int i = 1; i < argc; i++) {
+			for (int j = 0; argv[i][j]; j++) {
+				if (argv[i][j] >= 'a' && argv[i][j] <= 'z')
+					std::cout << (char)toupper(argv[i][j]);
+				else
+					if (argv[i][j] != '"')
+						std::cout << argv[i][j];
+			}
+		}
+		std::cout << std::endl;
+	}
+	return 0;
+}
