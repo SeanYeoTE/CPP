d871af03a0d8ba8ef3ca1b7433af9d83640faacd
sean <tseensean@gmail.com>
2025-02-20 17:22:37 +0800

    update

--- ex00/Megaphone.cpp
+++ ex00/Megaphone.cpp
@@ -1,31 +1,34 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   megaphone.cpp                                      :+:      :+:    :+:   */
+/*   Megaphone.cpp                                      :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: seayeo <seayeo@42.sg>                      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/01/22 13:53:41 by seayeo            #+#    #+#             */
-/*   Updated: 2025/01/22 14:10:11 by seayeo           ###   ########.fr       */
+/*   Updated: 2025/02/20 15:46:34 by seayeo           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <iostream>
 
-int main(int argc, char **argv) {
+int	main(int argc, char **argv)
+{
 	if (argc == 1)
 		std::cout << "* LOUD AND UNBEARABLE FEEDBACK NOISE *" << std::endl;
-	else {
-		for (int i = 1; i < argc; i++) {
-			for (int j = 0; argv[i][j]; j++) {
+	else
+	{
+		for (int i = 1; i < argc; i++)
+		{
+			for (int j = 0; argv[i][j]; j++)
+			{
 				if (argv[i][j] >= 'a' && argv[i][j] <= 'z')
 					std::cout << (char)toupper(argv[i][j]);
-				else
-					if (argv[i][j] != '"')
-						std::cout << argv[i][j];
+				else if (argv[i][j] != '"')
+					std::cout << argv[i][j];
 			}
 		}
 		std::cout << std::endl;
 	}
-	return 0;
+	return (0);
 }
