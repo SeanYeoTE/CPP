7d693daa16c01e9aac971b82504d3622d6ccbbe8
sean <tseensean@gmail.com>
2025-02-27 14:28:03 +0800

    added shell script for testing ex04

--- CPP01/ex04/main.cpp
+++ CPP01/ex04/main.cpp
@@ -6,7 +6,7 @@
 /*   By: seayeo <seayeo@42.sg>                      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/02/25 13:19:17 by seayeo            #+#    #+#             */
-/*   Updated: 2025/02/25 14:07:12 by seayeo           ###   ########.fr       */
+/*   Updated: 2025/02/27 14:04:32 by seayeo           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,23 +15,8 @@
 #include <string>
 #include <fstream>
 
-void openfile(std::string filename)
+int input_error_checks(int argc, char **argv)
 {
-    std::ifstream infile(filename.c_str());
-    if (!infile) {
-        std::cerr << "Error: Could not open file " << filename << std::endl;
-        return;
-    }
-    
-    int c = infile.get();
-    while (c != std::ifstream::traits_type::eof()) {
-        std::cout << c;
-        c = infile.get();
-    }
-    
-}
-
-int main(int argc, char **argv) {
     if (argc != 4) {
         std::cout << "Error: Invalid arguments" << std::endl;
         return 1;
@@ -40,6 +25,47 @@ int main(int argc, char **argv) {
     std::string s1 = argv[2];
     std::string s2 = argv[3];
 
-    openfile(filename);
+    if (s1.empty()) {
+        std::cout << "Error: Search string cannot be empty" << std::endl;
+        return 1;
+    }
+    
+    
+
+    return 0;
+}
+
+int main(int argc, char **argv) {
+    if (input_error_checks(argc, argv))
+        return 1;
+        
+    std::string filename = argv[1];
+    std::string s1 = argv[2];
+    std::string s2 = argv[3];
+
+    std::ifstream infile(filename.c_str());
+    if (!infile) {
+        std::cerr << "Error: Could not open file " << filename << std::endl;
+        return 1;
+    }
+    
+    std::string outfilename = filename + ".replace";
+    std::ofstream outfile(outfilename.c_str());
+    if (!outfile) {
+        std::cerr << "Error: Could not create output file " << outfilename << std::endl;
+        return 1;
+    }
+    
+    std::string line;
+    while (std::getline(infile, line)) {
+        size_t pos = 0;
+        while ((pos = line.find(s1, pos)) != std::string::npos) {
+            line = line.substr(0, pos) + s2 + line.substr(pos + s1.length());
+            pos += s2.length();
+        }
+        outfile << line;
+        if (!infile.eof()) outfile << '\n';
+    }
+    
     return 0;
 }
