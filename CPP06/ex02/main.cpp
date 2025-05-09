db3e77f907b856ce0440386433349f0d82c710d4
sean <tseensean@gmail.com>
2025-05-09 19:38:16 +0800

    exe name change
    ex02 dynamic cast for base *p,

---
+++ CPP06/ex02/main.cpp
@@ -0,0 +1,32 @@
+#include "base.hpp"
+
+Base *generate(void)
+{
+    srand(time(0));
+    int random = rand() % 3;
+    if (random == 0)
+        return new A();
+    else if (random == 1)
+        return new B();
+    else
+        return new C();
+}
+
+void identify(Base *p)
+{
+    if (dynamic_cast<A*>(p))
+        std::cout << "A" << std::endl;
+    else if (dynamic_cast<B*>(p))
+        std::cout << "B" << std::endl;
+    else if (dynamic_cast<C*>(p))
+        std::cout << "C" << std::endl;
+}
+int main()
+{
+    Base *base = generate();
+    identify(base);
+    // identify(*base);
+    delete base;
+    return 0;
+};
+
