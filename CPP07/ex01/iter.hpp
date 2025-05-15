3bf92d7d04d4f453fbcce6d05a8e74a3c2deeeb8
sean <tseensean@gmail.com>
2025-05-15 15:34:29 +0800

    added handling for const

--- CPP07/ex01/iter.hpp
+++ CPP07/ex01/iter.hpp
@@ -1,8 +1,23 @@
 #ifndef ITER_HPP
-#define ITER_HPP
+# define ITER_HPP
 
 #include <iostream>
 #include <string>
+
+template <typename T>
+void iter(T *array, int length, void (*func)(const T &))
+{
+    for (int i = 0; i < length; ++i)
+    {
+        func(array[i]);
+    }
+}
+
+template <typename T>
+void printElement(const T &element)
+{
+    std::cout << element << std::endl;
+}
 
 template <typename T>
 void iter(T *array, int length, void (*func)(T &))
