c00747e74d3eba9ecdca5714588b6337e0e055d9
sean <tseensean@gmail.com>
2025-11-02 18:46:21 +0800

    refactor: inline template functions in utils.hpp and remove utils.cpp
    - Moved implementations of printpairs, printchains, displayArray, and displayResults from utils.cpp to utils.hpp as inline definitions
    - Removed utils.cpp from SRCS in Makefile to avoid compilation issues with template functions
    - This ensures proper compilation of template functions by defining them in the header file

--- CPP09/ex02/utils.hpp
+++ CPP09/ex02/utils.hpp
@@ -15,15 +15,60 @@
 #define RESET "\033[0m"
 
 template <typename T>
-void printpairs(const T &pairs, int depth, int size);
+void printpairs(const T &pairs, int depth, int size) {
+  std::cout << CYAN << "Depth " << depth << ": " << RESET;
+  std::cout << YELLOW << "Pairs: " << RESET;
+  for (size_t i = 0; i < pairs.size(); i = i + size) {
+    std::cout << GREEN << "(" << RESET;
+    for (size_t j = i; j < i + size; ++j) {
+      if (j >= pairs.size())
+        break;
+      std::cout << pairs[j] << (j == i + size - 1 ? "" : ", ");
+    }
+    std::cout << GREEN << ") " << RESET;
+  }
+  std::cout << std::endl;
+}
 
 template <typename T>
-void printchains(const T &main_chain, const T &pend_chain, const T &remain_chain);
+void printchains(const T &main_chain, const T &pend_chain,
+                 const T &remain_chain) {
+  std::cout << BLUE << "Main chain: " << RESET;
+  for (size_t i = 0; i < main_chain.size(); ++i) {
+    std::cout << main_chain[i] << " ";
+  }
+  std::cout << "\n" << MAGENTA << "Pend chain: " << RESET;
+  for (size_t i = 0; i < pend_chain.size(); ++i) {
+    std::cout << pend_chain[i] << " ";
+  }
+  std::cout << "\n" << RED << "Remain chain: " << RESET;
+  for (size_t i = 0; i < remain_chain.size(); ++i) {
+    std::cout << remain_chain[i] << " ";
+  }
+  std::cout << std::endl;
+}
 
 template <typename T>
-void displayArray(const T &original, const T &data);
+void displayArray(const T &original, const T &data) {
+  std::cout << RED << "Before:" << RESET;
+  for (long unsigned int i = 0; i < original.size(); ++i) {
+    std::cout << " ";
+    std::cout << original[i];
+  }
+  std::cout << "\n" << GREEN << "After : " << RESET;
+  for (long unsigned int i = 0; i < data.size(); ++i) {
+    std::cout << " ";
+    std::cout << data[i];
+  }
+  std::cout << std::endl;
+}
 
 template <typename T>
-void displayResults(const T &original, double time, long comparisons, const std::string &containerType);
+void displayResults(const T &original, double time, long comparisons, const std::string &containerType) {
+  std::cout << "Time to process a range of " << original.size()
+            << " elements with std::" << containerType << " : " << time
+            << " us" << "\n"
+            << "Total comparisons: " << comparisons << std::endl;
+}
 
 #endif
